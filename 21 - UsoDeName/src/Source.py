70dcac4b3ea0304cce055f34d838ee73be0517e9
Victor-Tilve <ing.victortilve@gmail.com>
2021-03-02 21:24:41 -0500

    Uso de __name__

---
+++ 21 - UsoDeName/src/Source.py
@@ -0,0 +1,15 @@
+'''
+Created on 26/01/2021
+
+@author: VATS
+'''
+
+#!/usr/local/bin/python
+import os, sys
+
+def imprimir():
+    print("Estoy en la función principal")
+
+
+if __name__ == '__main__':
+    imprimir()
---
+++ 21 - UsoDeName/src/Source2.py
@@ -0,0 +1,11 @@
+'''
+Created on 26/01/2021
+
+@author: VATS
+'''
+
+def imprimir():
+    print("Estoy en la función principal")
+
+if __name__ == '__main__':
+    print("Hola")
