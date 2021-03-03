1d9f85224a6a9c863fa741cf6af90f246de78de3
Victor-Tilve <ing.victortilve@gmail.com>
2021-03-02 21:25:44 -0500

    Linear Regression

---
+++ 23 - ML - Linear Regression/src/Source.py
@@ -0,0 +1,40 @@
+'''
+Created on 9/02/2021
+
+@author: VATS
+'''
+import numpy as np
+from _ast import Return
+
+class LinearRegression(): 
+    
+    '''
+    classdocs
+    '''
+    
+    def __init__(self, lr=0.001, n_iters = 1000):    
+        '''
+        Constructor
+        '''
+        
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None #We use None to define, but not initialize the variable
+        self.bias = None
+        
+    def fit(self,X,y):   
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        
+        # gradient descent
+        for _ in range(self.n_iters):
+            y_predicted = np.dot(X,self.weights) + self.bias
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1/n_samples) * np.sum(y_predicted - y)
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
+    
+    def predict(self, X):
+        y_predicted = np.dot(X,self.weights) + self.bias
+        return y_predicted 
