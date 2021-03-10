4365b0eea2a5c7d2e0f1de7a899d237d901ce139
Victor Tilve <ing.victortilve@gmail.com>
2021-03-10 14:52:26 -0500

    reading XML
    Co-Authored-By: Victor-Tilve <72095010+Victor-Tilve@users.noreply.github.com>

---
+++ 26 - Reading XML Documents/readingXML.py
@@ -0,0 +1,28 @@
+from xml.dom import minidom
+
+# parse an xml file by name
+mydoc = minidom.parse('items.xml')
+
+items = mydoc.getElementsByTagName('item')
+
+#Type
+print("Items' Type:" + str(type(items)))
+
+# one specific item attribute
+print('Item #1 attribute:')
+print(items[0].attributes['name'].value)
+
+# all item attributes
+print('\nAll attributes:')
+for elem in items:
+    print(elem.attributes['name'].value)
+
+# one specific item's data
+print('\nItem #2 data:')
+print(items[1].firstChild.data)
+print(items[1].childNodes[0].data)
+
+# all items data
+print('\nAll item data:')
+for elem in items:
+    print(elem.firstChild.data)
