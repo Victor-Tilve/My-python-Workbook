049d33c0a05ad2a02d62e0b77f7e7427cef84728
Victor-Tilve <ing.victortilve@gmail.com>
2021-06-21 07:21:43 -0500

    Update

---
+++ 30 - SerialTerminal-and-Parser/ReadingXML.py
@@ -0,0 +1,40 @@
+from xml.dom import minidom
+
+
+''' 
+En este archivo puedo instanciar el objeto "Serial", de tal forma que la funcion retorne el objeto
+'''
+# parse an xml file by name
+mydoc = minidom.parse('config.xml')
+
+items = mydoc.getElementsByTagName('item')
+pruebas = mydoc.getElementsByTagName('prueb')
+
+# all item attributes
+print('\nAll attributes:')
+for elem in items:
+    print(elem.firstChild.data)
+
+
+
+"""#Type
+print("Items' Type:" + str(type(items)))
+
+# one specific item attribute
+print('Item #1 attribute:')
+print(items[0].attributes['name'].value)
+
+# all item attributes
+print('\nAll attributes:')
+for elem in items:
+    print(elem.attributes['name'].value)
+
+# one specific item's data
+print('\nItem #2 data:')
+print(items[1].firstChild.data)
+print(items[1].childNodes[0].data)
+
+# all items data
+print('\nAll item data:')
+for elem in items:
+    print(elem.firstChild.data)"""
---
+++ 30 - SerialTerminal-and-Parser/SerialPort.py
@@ -0,0 +1,33 @@
+from serial import LineReader
+from serial import ReaderThread
+import serial
+'''
+En vez de instanciar directamente el objeto, llamo a una funcion que gestione los parametros del objeto desde un archivo de configuración
+'''
+# ser = serial.Serial()
+# ser.baudrate = 9600
+# ser.port = 'COM11'
+# print(ser)
+# ser.open()
+# ser.write(b'Hello!')
+# # print(serial.tools.list_ports)
+
+
+class PrintLines(LineReader):
+    def connection_made(self, transport):
+        super(PrintLines, self).connection_made(transport)
+        sys.stdout.write('port opened\n')
+        self.write_line('hello world')
+
+    def handle_line(self, data):
+        sys.stdout.write('line received: {}\n'.format(repr(data)))
+
+    def connection_lost(self, exc):
+        if exc:
+            traceback.print_exc(exc)
+        sys.stdout.write('port closed\n')
+
+ser = serial.serial_for_url('loop://', baudrate=9600, timeout=1)
+with ReaderThread(ser, PrintLines) as protocol:
+    protocol.write_line('hello')
+    time.sleep(2)
